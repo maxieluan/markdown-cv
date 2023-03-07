259072203e27052ac65df78ec894bed80fba3b61
Max Luan <max_luan@foxmail.com>
2023-03-08 02:57:40 +0800

    wrote a function for parsing and generating
    tweaked font, styles and alignment
    
    TODO: Hook up to web ui
    
    TODO: add styles, configures, and chinese font support.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import PDFDocument from 'pdfkit';
 var blobStream = require('blob-stream');
 import "./register-files"
 import EasyMDE from "easymde";
+import {marked} from "marked";
 
 const editor = document.getElementById('markdown-input');
 const easyMDE = new EasyMDE({
@@ -22,7 +23,7 @@ var lorem =
 
 function setupEditor() {
   const renderePDFDebounce = debounce(() => {
-    const markdown = editor.value;
+    const markdown = easyMDE.value();
     const pdf = generatePdf(markdown);
   }, 500);
 
@@ -52,53 +53,12 @@ function generatePdf(markdown) {
 
   doc.registerFont('Roboto', 'fonts/Roboto-Regular.ttf')
 
-  // draw some text
-  doc.fontSize(25).text('Here is some vector graphics...', 100, 80);
+  doc.info.Title = 'My CV';
 
-  // some vector graphics
-  doc
-    .save()
-    .moveTo(100, 150)
-    .lineTo(100, 250)
-    .lineTo(200, 250)
-    .fill('#FF3300');
+  
+  const ast = marked.lexer(markdown);
+  console.log(ast);
 
-  doc.circle(280, 200, 50).fill('#6600FF');
-
-  // an SVG path
-  doc
-    .scale(0.6)
-    .translate(470, 130)
-    .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
-    .fill('red', 'even-odd')
-    .restore();
-
-  // and some justified text wrapped into columns
-  doc
-    .font('Roboto')
-    .text('And here is some wrapped text...', 100, 300)
-    .fontSize(13)
-    .moveDown()
-    .text(lorem, {
-      width: 412,
-      align: 'justify',
-      indent: 30,
-      columns: 2,
-      height: 300,
-      ellipsis: true
-    });
-
-  doc.addPage();
-
-  doc
-    .fontSize(25)
-    .font('Courier')
-
-  doc
-    .font('Courier-Bold')
-    .text('Finish...')
-
-  // end and display the document in the iframe to the right
   doc.end();
   stream.on('finish', function () {
     iframe.src = stream.toBlobURL('application/pdf');
